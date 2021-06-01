0fdb412f0a6418d4cf7b6ead7f7b250156340334
Caroline Byström <frivridnadagar@gmail.com>
2021-06-01 14:43:37 +0200

    start frontend with form to submit a name and try to connect it to socket

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:4000')
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js! Caroline Fetgyukkag 
-    </div>
-  )
+
+const [name, setName] = useState('')
+
+useEffect(() => {
+	socket.on(name => {
+    setName(name)
+	})
+})
+
+const onSubmit = (e) => {
+	e.preventDefault()
+	socket.emit(name)
+  setName('')
+}
+
+const onTextChange = e => {
+	setName(e.target.value)
+}
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<h1> Name/Role: </h1>
+				<div>
+						<textarea
+							name="name" 
+							onChange={e => onTextChange(e)} 
+							value={name}
+            />
+				</div>
+					<button type="submit">START SESSION!</button>
+				</form>
+		</div>
+	)
 }
