e4fa7eed9922003904b135bf3f4c742e0b8e7ddb
Caroline Byström <frivridnadagar@gmail.com>
2021-06-01 15:41:42 +0200

    start with clock example socket.io

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import io from 'socket.io-client'
-
-const socket = io.connect('http://localhost:4000')
+import socketIOClient from "socket.io-client"
+//const ENDPOINT = "http://192.168.1.145:4001" //127.0.0.1
+//const ENDPOINT = "http://http://172.25.145.136:4001"
+const ENDPOINT = "http://localhost:4001"
 
 export const App = () => {
+  const [response, setResponse] = useState("");
 
-const [name, setName] = useState('')
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    //const socket = io.connect('http://localhost:4001')
+    socket.on("FromAPI", data => {
+      setResponse(data)
+    });
+    // Clean up the effect
+    return () => socket.disconnect();
+  }, []);
 
-useEffect(() => {
-	socket.on(name => {
-    setName(name)
-	})
-})
-
-const onSubmit = (e) => {
-	e.preventDefault()
-	socket.emit(name)
-  setName('')
-}
-
-const onTextChange = e => {
-	setName(e.target.value)
-}
-
-	return (
-		<div>
-			<form onSubmit={onSubmit}>
-				<h1> Name/Role: </h1>
-				<div>
-						<textarea
-							name="name" 
-							onChange={e => onTextChange(e)} 
-							value={name}
-            />
-				</div>
-					<button type="submit">START SESSION!</button>
-				</form>
-		</div>
-	)
+  return (
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
+  );
 }
