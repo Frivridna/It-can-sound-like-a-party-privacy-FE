516d0e9b1ce4ad32bcdb23f2dc22a53d9cff2320
Caroline Byström <frivridnadagar@gmail.com>
2021-06-02 18:46:17 +0200

    remove file outside of src

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React, { useState, useEffect } from 'react'
 import socketIOClient from "socket.io-client"
 //const ENDPOINT = "http://192.168.1.145:4001" //127.0.0.1
 //const ENDPOINT = "http://http://172.25.145.136:4001"
-const ENDPOINT = "http://localhost:4001"
+const ENDPOINT = "http://localhost:4001" // add to reusable folder 
 
 export const App = () => {
-  const [response, setResponse] = useState(""); // {} receive sound file in a useState react ???? 
-
-  //let audio = new Audio() // ta emot från backend på något sätt
+  const [response, setResponse] = useState("")
 
   let audio = new Audio(response)
 
@@ -18,11 +16,8 @@ export const App = () => {
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
-    //const socket = io.connect('http://localhost:4001') 
     socket.on("FromAPI", data => {
-      //console.log(data)
       setResponse(data)
-      //console.log(response)
       playAudio()
     });
   }, [playAudio, response]);
@@ -31,7 +26,6 @@ export const App = () => {
   return (
     <>
     <p>
-      {/* It's <time dateTime={response}>{response}</time> */}
       Test
     </p>
     <button onClick={() => playAudio()}>button</button> 
