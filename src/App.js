4bf587348e7d1a6330cfc5acf6025de19a5416b8
Caroline Byström <frivridnadagar@gmail.com>
2021-06-04 11:22:47 +0200

    receive sound from backend

--- src/App.js
+++ src/App.js
@@ -3,32 +3,42 @@ import socketIOClient from "socket.io-client"
 //const ENDPOINT = "http://192.168.1.145:4001" //127.0.0.1
 //const ENDPOINT = "http://http://172.25.145.136:4001"
 const ENDPOINT = "http://localhost:4001" // add to reusable folder 
+const socket = socketIOClient(ENDPOINT)
 
 export const App = () => {
   const [response, setResponse] = useState("")
 
-  let audio = new Audio(response)
+  
 
   const playAudio = (response) => {
+    console.log('Play audio: ' + response)
+    if (response) {
+    let audio = new Audio(response)
     audio.play(response)
+    }
   }
   
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("FromAPI", response => {
-      console.log(response)
       setResponse(response)
-      playAudio()
+      console.log('From API: ', + response)
+      playAudio(response)
     });
   }, [playAudio, response]);
 
+  const sendSocketClick = () => {
+    console.log('Sends a click')
+    socket.emit('click', true)
+  }
+  
 
   return (
     <>
     <p>
       Test
     </p>
-    <button onClick={() => playAudio()}>button</button> 
+    <button onClick={() => sendSocketClick()}>button</button> 
     </>
   );
 }
