b5d9645cd74d8b92813c1916e952a6a5d30516e2
Caroline Byström <frivridnadagar@gmail.com>
2021-06-17 14:37:28 +0200

    something for FinishPage

---
+++ src/components/FinishPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+
+
+const FinishPage = () => {
+  
+  return (
+    <Paper elevation={3} />
+  )
+}
+
+export const FinishPage
