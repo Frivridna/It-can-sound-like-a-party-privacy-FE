e12280b96f729a4c05189c4e5c5cd26a1b497339
Caroline Byström <frivridnadagar@gmail.com>
2021-06-22 09:15:17 +0200

    change back to run on localhost

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,11 +1,11 @@
-export const ENDPOINT = "https://icslap-p.herokuapp.com/"
+export const ENDPOINT = "http://localhost:4001"
 //export const ENDPOINT = "https://icslap-p.herokuapp.com/"
 
 //export const API_URL = "https://icslap-p.herokuapp.com/signin"
 
-export const BASE_URL = "https://icslap-p.herokuapp.com/"
+export const BASE_URL = "http://localhost:4001"
 
 export const API_URL = (slug) => `${BASE_URL}${slug}`
 
-export const DELETE_URL = (_id) => `https://icslap-p.herokuapp.com/sounds/${_id}`
+export const DELETE_URL = (_id) => `http://localhost:4001/sounds/${_id}`
 //http://localhost:4001
