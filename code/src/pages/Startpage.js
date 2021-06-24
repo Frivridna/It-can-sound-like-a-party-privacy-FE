4f8c1c15d5756c3e2ffa2b3629d2652ebbecd6bc
Caroline Byström <frivridnadagar@gmail.com>
2021-06-24 12:28:24 +0200

    add footer text on Startpage

--- code/src/pages/Startpage.js
+++ code/src/pages/Startpage.js
@@ -31,7 +31,7 @@ const Startpage = ({ status }) => {
     <section className="startpage">
         <header className="header">
             <h1 className="header-text">It Can Sound Like a Party, Privacy</h1>
-            <h5 className="intro-text">An immersive artwork for two people</h5>
+            <h5 className="intro-text">An voice and sound artwork for two people</h5>
         </header>
         <div className="image-container">
             <img className="startpage-image" src="../assets/blood.jpg" alt="lungs" />
@@ -41,6 +41,17 @@ const Startpage = ({ status }) => {
                 <button className="start-button">START</button>
             </Link>
         </div>
+        <footer classname="footer">
+            <p className="footer-text">
+                Demo June 2021. In it's extension this project will have voice material directed to user A, and to user B. As for now you will only hear two different sound compositions. 
+            </p>
+            <p className="footer-text">
+                Coders: Fethullah Dincer and Caroline Byström. 
+            </p>
+            <p className="footer-text">
+                Sound: Franz Edvard Cedrins.
+            </p>
+        </footer>
     </section>
     }
   </>
